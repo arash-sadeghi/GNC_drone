b6114ef37e375598137064e751a6046711a4bf78
Arash Sadeghi <arash.sadeghi1997@gmail.com>
2023-08-11 02:52:59 -0230

    a naive yaw controller added. drone now works way better. but this approach is naive because model is still controlled linearly and yaw is as if controlled seperately

--- src/GNC/Controller.py
+++ src/GNC/Controller.py
@@ -4,6 +4,7 @@ import rospy
 from geometry_msgs.msg import Twist , Pose
 import numpy as np 
 import control as ct
+from tf.transformations import euler_from_quaternion,quaternion_from_euler
 
 class LQR:
 	def __init__(self) -> None:
@@ -15,19 +16,25 @@ class LQR:
 		self.Xref = None
 		self.X = None
 		self.K = None
+		self.state_dim = 4
+		self.input_dim = 4
 
 	def pose_callback(self,data):
-		self.X = np.array([data.position.x, data.position.y, data.position.z])
+		orientation_list = [data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w]
+		(roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+		self.X = np.array([data.position.x, data.position.y, data.position.z , yaw])
 	
 	def Xref_callback(self,data):
-		self.Xref = np.array([data.position.x, data.position.y, data.position.z])
+		orientation_list = [data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w]
+		(roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+		self.Xref = np.array([data.position.x, data.position.y, data.position.z , yaw])
 		
 	def calculate_K(self):
-		A = np.zeros((3,3))
-		B = np.eye(3)
+		A = np.zeros((self.state_dim,self.state_dim)) #! this is not true anymore
+		B = np.eye(self.input_dim)
 
-		Q = 1.0*np.eye(3)
-		R =  0.1*np.identity(3)
+		Q = 1.0*np.eye(self.state_dim)
+		R =  0.1*np.identity(self.input_dim)
 
 		K, S, E = ct.lqr(A, B, Q,R)
 		A_new=(A-B@K)
@@ -49,6 +56,8 @@ class LQR:
 		twist.linear.x = U[0] 
 		twist.linear.y = U[1] 
 		twist.linear.z = U[2] 
+		twist.angular.z = U[3]
+
 
 		self.vel_pub.publish(twist)
 
--- src/GNC/Planner.py
+++ src/GNC/Planner.py
@@ -2,6 +2,7 @@
 
 import rospy
 from geometry_msgs.msg import Twist , Pose
+from tf.transformations import quaternion_from_euler
 
 class Dummy:
     def __init__(self) -> None:
@@ -14,6 +15,12 @@ class Dummy:
         Xref.position.x = 0
         Xref.position.y = 0
         Xref.position.z = 3
+        yaw_ref = 0
+        quaternion_ref = quaternion_from_euler(0,0,yaw_ref)
+        Xref.orientation.x = quaternion_ref[0]
+        Xref.orientation.y = quaternion_ref[1]
+        Xref.orientation.z = quaternion_ref[2]
+        Xref.orientation.w = quaternion_ref[3]
         self.Xref_publisher.publish(Xref)
 
 if __name__=="__main__":
